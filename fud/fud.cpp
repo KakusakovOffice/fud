1d661b8e57beb06736d6b0cbed59b093c4a9792d
KakusakovOffice <99006858+KakusakovOffice@users.noreply.github.com>
2024-06-24 17:04:42 +0300

    Add files via upload

--- fud/fud.cpp
+++ fud/fud.cpp
@@ -1,5 +1,3 @@
-// <адрес заказа> <тип заказа> <цена заказа> <вес> <дата>
-
 #include "stdafx.h"
 
 #include <cstdio>
@@ -13,27 +11,31 @@ using namespace std;
 using namespace System;
 using namespace System::IO;
 
-struct client
+// Структура описывающая один заказ еды.
+struct order
 {
-	char address[20];
-	char kind[20];
-	char date[11];
-	long weight;
-	long price;
+	char address[20];  // Адрес заказа.
+	char kind[20];  // Тип продукта.
+	char date[11];  // Дата поступления заказа.
+	long weight;  // Вес заказа.
+	long price;  // Цена заказа.
 };
 
+// Двусторонний связный список, хранящий 
+// данные о суммарной цене и кол-ве 
+// заказов определенного типа.
 struct list
 {
-	char kind[20];
-	long count;
-	long price;
-	struct list* next;
-	struct list* prev;
+	char kind[20];  // Тип продукта
+	long count;  // Количество заказов с продуктом.
+	long price;  // Суммарная цена заказов.
+	struct list* next;  // Следующий узел списка.
+	struct list* prev;  // Предыдущий узел списказ.
 };
 
-unsigned int count;
-struct client* clients;
-struct list* sp;
+unsigned int count;  // Количество заказов еды.
+struct order* orders;  // Массив заказов еды.
+struct list* sp;  // Список заказов еды по типу.
 
 #define ENTER 13
 #define ESC   27
@@ -42,7 +44,10 @@ struct list* sp;
 #define HOME  71
 #define END   79
 
-void print_clients()
+// Описание:
+// Печатает в консоль (переписывает весь экран)
+// данные массива orders.
+void print_orders()
 {
 	Console::ForegroundColor = ConsoleColor::White;
 	Console::BackgroundColor = ConsoleColor::Black;
@@ -51,24 +56,27 @@ void print_clients()
 	{
 		printf(
 			"\n%-20s %-20s %11s %7ld %7ld",
-			clients[i].address,
-			clients[i].kind,
-			clients[i].date,
-			clients[i].weight,
-			clients[i].price
+			orders[i].address,
+			orders[i].kind,
+			orders[i].date,
+			orders[i].weight,
+			orders[i].price
 		);
 	}
-	_getch();
+	(void)_getch();
 }
 
-void print_most_expensive()
+// Описание:
+// Печатает в консоль (после курсора)
+// заказ с наибольшей ценой.
+void print_most_expensive_order()
 {
-	struct client* best = &clients[0];
-	for (unsigned int i = 0; i < count; i++)
+	struct order* best = &orders[0];
+	for (unsigned int i = 1; i < count; i++)
 	{
-		if (best->price < clients[i].price)
+		if (best->price < orders[i].price)
 		{
-			best = &clients[i];
+			best = &orders[i];
 		}
 	}
 	Console::ForegroundColor = ConsoleColor::Yellow;
@@ -78,9 +86,25 @@ void print_most_expensive()
 	printf("Самый дорогой заказ: %ld руб.\n", best->price);
 	Console::CursorLeft = 10;
 	printf("Его адрес: %s\n", best->address);
-	_getch();
+	(void)_getch();
 }
 
+// Описание:
+// Переводит дату из формата 'гггг_мм_дд' (from)
+// в формат 'дд месяц гггг' (to).
+// 
+// Если участки памяти to и from пересекаются, 
+// то поведение функции неопределено.
+// 
+// Аргументы:
+// to   -> Указатель на массив (24+ симв.), 
+//         в котором будет сохранена дата в 
+//         итоговом формате как 
+//         нуль-терминированная строка.
+// from -> Указатель на нуль-терминированную 
+//         строку (11 симв. включая нуль),
+//         в которой содержится дата в 
+//         изначальном формате.
 void to_pretty_date(char* to, const char* from)
 {
 	char tmp[3];
@@ -97,14 +121,20 @@ void to_pretty_date(char* to, const char* from)
 	strncat(to, from, 4);
 }
 
-void last()
+// Описание:
+// Печатает в консоль (после курсора)
+// заказ с самой поздней датой.
+//
+// Если заказов с такой датой несколько, 
+// то печатает любой из них.
+void last_order()
 {
-	struct client* best = &clients[0];
+	struct order* best = &orders[0];
 	for (unsigned int i = 0; i < count; i++)
 	{
-		if (strcmp(clients[i].date, best->date) < 0)
+		if (strcmp(orders[i].date, best->date) < 0)
 		{
-			best = &clients[i];
+			best = &orders[i];
 		}
 	}
 
@@ -122,15 +152,18 @@ void last()
 	printf("тип: %s\n", best->kind);
 	Console::CursorLeft = 12;
 	printf("сумма: %ld\n", best->price);
-	_getch();
+	(void)_getch();
 }
 
-void print_pizza_count()
+// Описание:
+// Печатает в консоль (после курсора)
+// количество заказов пиццы.
+void print_pizza_order_count()
 {
 	unsigned int total = 0;
 	for (unsigned int i = 0; i < count; i++)
 	{
-		if (strcmp(clients[i].kind, "пицца") == 0) total++;
+		if (strcmp(orders[i].kind, "пицца") == 0) total++;
 	}
 
 	Console::ForegroundColor = ConsoleColor::Yellow;
@@ -138,52 +171,90 @@ void print_pizza_count()
 
 	Console::CursorLeft = 10;
 	printf("\nКоличество заказов пиццы: %u", total);
-	_getch();
+	(void)_getch();
 }
 
-void make_list()
+// Описание:
+// Выдкляет память и записывает новый узел списка,
+// с пустыми указателями на следующий и предыдущий
+// узлы.
+//
+// Аргументы:
+// order -> Единственный заказ который будет
+//          описываться узлом.
+// 
+// Возвращает:
+// Новый узел списка. Чтобы освободить память
+// можно использовать free().
+struct list* create_list_node(struct order order)
 {
-	if (sp != NULL) return;
-	sp = (struct list*)malloc(sizeof(struct list));
-	if (sp == NULL) abort();
-	strcpy(sp->kind, clients[0].kind);
-	sp->price = clients[0].price;
-	sp->count = 1;
-	sp->next = NULL;
+	struct list* node = (struct list*)malloc(sizeof(struct list));
+	if (!node) abort();
+	strcpy(node->kind, order.kind);
+	node->count = 1;
+	node->price = order.price;
+	node->next = NULL;
+	node->prev = NULL;
+	return node;
+}
 
-	for (unsigned int i = 1; i < count; i++)
+// Описание:
+// Добавляет заказ в список sp сортируя его по типу 
+// заказа, при надобности выделяя память.
+//
+// Функция гарантирует, что sp всегда будет 
+// указывать на начало списка.
+//
+// Если порядок сортировки списка sp был нарушен,
+// то поведение функции неопределено.
+//
+// Аргументы:
+// order -> Заказ, который будет добавлен в список sp.
+void insert_order(struct order order)
+{
+	if (!sp)
 	{
-		struct list* nt;
-		struct list* prev = NULL;
-		for (nt = sp;
-			nt != NULL && strcmp(clients[i].kind, nt->kind) > 0;
-			prev = nt, nt = nt->next
-			);
-		if (strcmp(nt->kind, clients[i].kind) == 0)
-		{
-			nt->price += clients[i].price;
-			nt->count++;
-			continue;
-		}
-
-		struct list* tmp = (struct list*)malloc(sizeof(struct list));
-		if (tmp == NULL) abort();
-		strcpy(tmp->kind, clients[i].kind);
-		tmp->price = clients[i].price;
-		tmp->count = 1;
-		tmp->next = nt;
-		if (prev != NULL) prev->next = tmp;
-		else  sp = tmp;
+		sp = create_list_node(order);
+		return;
 	}
-	sp->prev = NULL;
-	struct list* tail = sp;
-	while (tail->next != NULL)
+
+	struct list* node = sp;
+	while (true)
 	{
-		tail->next->prev = tail;
-		tail = tail->next;
+		int cmp = strcmp(order.kind, node->kind);
+		if (cmp == 0) 
+		{
+			node->count++;
+			node->price += order.price;
+			return;
+		}
+		else if (cmp > 0)
+		{
+			if (!node->next)
+			{
+				node->next = create_list_node(order);
+				node->next->prev = node;
+				return;
+			}
+			node = node->next;
+		}
+		else
+		{
+			struct list* tmp = node->prev;
+			node->prev = create_list_node(order);
+			node->prev->next = node;
+			node->prev->prev = tmp;
+			if (tmp) tmp->next = node->prev;
+			if (sp->prev) sp = sp->prev;
+			return;
+		}
 	}
 }
 
+// Описание:
+// Печатает в консоль (переписывает весь экран)
+// данные списка sp в алфавитном порядке 
+// (сортировка идет по типу заказа).
 void list_alpha()
 {
 	Console::ForegroundColor = ConsoleColor::Black;
@@ -193,7 +264,6 @@ void list_alpha()
 	Console::CursorLeft = 12;
 	printf("Алфавитный список по типу заказа.");
 	printf("\n\r----------------------------------------------------------------");
-	make_list();
 	for (struct list* nt = sp; nt != NULL; nt = nt->next)
 	{
 		printf(
@@ -203,9 +273,13 @@ void list_alpha()
 			nt->price
 		);
 	}
-	_getch();
+	(void)_getch();
 }
 
+// Описание:
+// Печатает в консоль (переписывает весь экран)
+// данные списка sp в обратном алфавитном порядке
+// (сортировка идет по типу заказа).
 void list_alpha_reverse()
 {
 	Console::ForegroundColor = ConsoleColor::Black;
@@ -215,7 +289,6 @@ void list_alpha_reverse()
 	Console::CursorLeft = 12;
 	printf("Обратный алфавитный список по типу заказа.");
 	printf("\n\r----------------------------------------------------------------------------");
-	make_list();
 	struct list* tail = sp;
 	while (tail->next != NULL) tail = tail->next;
 	for (struct list* nt = tail; nt != NULL; nt = nt->prev)
@@ -227,9 +300,13 @@ void list_alpha_reverse()
 			nt->price
 		);
 	}
-	_getch();
+	(void)_getch();
 }
 
+// Описание:
+// Печатает в консоль (переписывает весь экран)
+// список заказов из массива orders, цена 
+// которых превышает 1000.
 void print_expensive_list()
 {
 	Console::ForegroundColor = ConsoleColor::Black;
@@ -239,23 +316,26 @@ void print_expensive_list()
 	Console::CursorLeft = 12;
 	printf("Список заказов на сумму более 1.000 руб");
 	printf("\n\r----------------------------------------------------------------");
-	make_list();
 	for (int i = 0; i < count; i++)
 	{
-		if (clients[i].price > 1000)
+		if (orders[i].price > 1000)
 		{
 			printf(
 				"%\n\r%-20s %-20s %7ld г %7ld р.",
-				clients[i].address,
-				clients[i].kind,
-				clients[i].weight,
-				clients[i].price
+				orders[i].address,
+				orders[i].kind,
+				orders[i].weight,
+				orders[i].price
 			);
 		}
 	}
-	_getch();
+	(void)_getch();
 }
 
+// Описание:
+// Печатает в консоль (переписывает весь экран)
+// диаграмму по наиболее дорогим заказам,
+// основанную на данных списка sp.
 void diagram()
 {
 	Console::BackgroundColor = ConsoleColor::Yellow;
@@ -263,10 +343,9 @@ void diagram()
 	long total = 0;
 	for (unsigned int i = 0; i < count; i++)
 	{
-		total += clients[i].price;
+		total += orders[i].price;
 	}
 
-	make_list();
 	ConsoleColor color = ConsoleColor::Black;
 	int ncolor = 0;
 	for (struct list* nt = sp; nt != NULL; nt = nt->next, ncolor++, color++)
@@ -286,23 +365,28 @@ void diagram()
 			ncolor = 0;
 		}
 	}
-	_getch();
+	(void)_getch();
 	return;
 }
 
+// Описание:
+// Печатает в консоль (после курсора)
+// первое совпадение цен заказа для
+// двух заказов из массива orders с 
+// разными типами.
 void match_one()
 {
 	Console::ForegroundColor = ConsoleColor::Yellow;
 	Console::BackgroundColor = ConsoleColor::Black;
 
-	struct client a, b;
+	struct order a, b;
 	bool found_match = false;
 	for (unsigned int i = 0; i < count; i++)
 	{
 		for (unsigned int j = i + 1; j < count; j++)
 		{
-			a = clients[i];
-			b = clients[j];
+			a = orders[i];
+			b = orders[j];
 			if (strcmp(a.kind, b.kind) == 0) continue;
 			if (a.price != b.price) continue;
 			found_match = true;
@@ -322,9 +406,14 @@ finish:
 		printf("тип 2: %-20s\n", b.kind);
 	}
 	else printf("Совпадений не найдено.\n");
-	_getch();
+	(void)_getch();
 }
 
+// Описание:
+// Печатает в консоль (переписывает весь экран)
+// все совпадения цен заказа для
+// двух заказов из массива orders с 
+// разными типами.
 void match()
 {
 	Console::ForegroundColor = ConsoleColor::Black;
@@ -335,16 +424,20 @@ void match()
 	{
 		for (unsigned int j = i + 1; j < count; j++)
 		{
-			struct client a = clients[i];
-			struct client b = clients[j];
+			struct order a = orders[i];
+			struct order b = orders[j];
 			if (strcmp(a.kind, b.kind) == 0) continue;
 			if (a.price != b.price) continue;
 			printf("\r\n%-20s %-20s %7ld", a.kind, b.kind, a.price);
 		}
 	}
-	_getch();
+	(void)_getch();
 }
 
+// Описание:
+// Отрисовывает меню и позволяет выбрать 
+// одну из функций приложения при помощи
+// клавиатуры.
 void menu()
 {
 	const char* options[] = {
@@ -405,19 +498,19 @@ void menu()
 		case ENTER:
 			switch (y)
 			{
-			case 1: print_most_expensive(); return;
-			case 2: last();                 return;
-			case 3: list_alpha();           return;
-			case 4: list_alpha_reverse();   return;
-			case 5: print_expensive_list(); return;
-			case 6: print_pizza_count();    return;
-			case 7: diagram();              return;
-			case 8: match();                return;
-			case 9: match_one();            return;
-			case 10: print_clients();        return;
+			case 1: print_most_expensive_order(); return;
+			case 2: last_order();                 return;
+			case 3: list_alpha();                 return;
+			case 4: list_alpha_reverse();         return;
+			case 5: print_expensive_list();       return;
+			case 6: print_pizza_order_count();    return;
+			case 7: diagram();                    return;
+			case 8: match();                      return;
+			case 9: match_one();                  return;
+			case 10: print_orders();              return;
 			case 11: exit(0);
 			}
-		case ESC:   exit(0);
+		case ESC:    exit(0);
 		}
 
 		if (y > last_option) y = first_option;
@@ -432,22 +525,22 @@ void make_file(const char* path)
 		file,
 		"15\n"
 		"ул_Ленина_д10 яблоки        2024-11-12 5000 2000\n"
-		"ул_Сталина_д3 пицца         2024-11-11 500 2500\n"
-		"ул_Пушкина_д3 суповой_набор 2024-11-12 250 500\n"
-		"ул_Пушкина_д2 мороженное    2024-11-12 100 400\n"
-		"д_Колотушкина пицца         2024-11-11 950 6500\n"
-		"ул_Ввод_д6    суши          2024-11-10 500 2500\n"
-		"ул_Кукина_д3  шоколад       2024-11-11 350 2000\n"
+		"ул_Сталина_д3 пицца         2024-11-11 500  2500\n"
+		"ул_Пушкина_д3 суповой_набор 2024-11-12 250  500\n"
+		"ул_Пушкина_д2 мороженное    2024-11-12 100  400\n"
+		"д_Колотушкина пицца         2024-11-11 950  6500\n"
+		"ул_Ввод_д6    суши          2024-11-10 500  2500\n"
+		"ул_Кукина_д3  шоколад       2024-11-11 350  2000\n"
 
 		"Кремль        шоколад       2024-12-11 1000 10000\n"
 
 		"ул_Ленина_д12 капуста       2024-11-12 2250 250\n"
-		"ул_Сталина_д7 пицца         2024-11-11 500 2500\n"
-		"ул_Пушкина_д4 сыр_Гауда     2024-11-12 250 500\n"
-		"ул_Пушкина_д7 мороженное    2024-11-12 100 400\n"
-		"ул_Пушкина_д9 суповой_набор 2024-11-11 100 400\n"
-		"ул_Ввод_д8    шоколад       2024-11-13 250 1250\n"
-		"ул_Кукина_д5  суши          2024-11-11 700 5000\n"
+		"ул_Сталина_д7 пицца         2024-11-11 500  2500\n"
+		"ул_Пушкина_д4 сыр_коровий   2024-11-12 250  500\n"
+		"ул_Пушкина_д7 мороженное    2024-11-12 100  400\n"
+		"ул_Пушкина_д9 суповой_набор 2024-11-11 100  400\n"
+		"ул_Ввод_д8    шоколад       2024-11-13 250  1250\n"
+		"ул_Кукина_д5  суши          2024-11-11 700  5000\n"
 	) < 0) exit(1);
 	fclose(file);
 }
@@ -458,6 +551,7 @@ int main(array<System::String^>^ args)
 	Console::CursorVisible::set(false);
 	Console::BufferHeight = Console::WindowHeight;
 	Console::BufferWidth = Console::WindowWidth;
+
 	const char* path = "Vklad.dat";
 	make_file(path);
 	FILE* file = fopen(path, "r");
@@ -467,7 +561,7 @@ int main(array<System::String^>^ args)
 			"\nошибка: Не удается открыть файл %s "
 			"для чтения: %s", path, strerror(errno)
 		);
-		_getch();
+		(void)_getch();
 		exit(1);
 	}
 
@@ -477,19 +571,19 @@ int main(array<System::String^>^ args)
 			"\nошибка: файл должен начинаться "
 			"количеством записей (положительное число)"
 		);
-		_getch();
+		(void)_getch();
 		exit(1);
 	}
 
 	if (count == 0)
 	{
 		printf("\nошибка: количество записей не может быть нулевым");
-		_getch();
+		(void)_getch();
 		exit(1);
 	}
 
-	clients = (struct client*)malloc(sizeof(struct client) * count);
-	if (clients == NULL) abort();
+	orders = (struct order*)malloc(sizeof(struct order) * count);
+	if (orders == NULL) abort();
 	for (unsigned int i = 0; i < count; i++)
 	{
 		if (feof(file))
@@ -499,25 +593,26 @@ int main(array<System::String^>^ args)
 				"чем объявлено в его заголовке "
 				"(%u из %u)", i, count
 			);
-			_getch();
+			(void)_getch();
 			exit(1);
 		}
 		if (fscanf(
 			file,
 			"%s%s%s%ld%ld",
-			clients[i].address,
-			clients[i].kind,
-			clients[i].date,
-			&clients[i].weight,
-			&clients[i].price) != 5)
+			orders[i].address,
+			orders[i].kind,
+			orders[i].date,
+			&orders[i].weight,
+			&orders[i].price) != 5)
 		{
 			printf(
 				"\nошибка: запись номер %u повреждена или "
 				"записана в некорректном формате", i + 1
 			);
-			_getch();
+			(void)_getch();
 			exit(1);
 		}
+		insert_order(orders[i]);
 	}
 
 	while (1) menu();
